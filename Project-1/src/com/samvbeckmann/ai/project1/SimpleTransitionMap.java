bd493a36d1486f928c59d7d1cd4ab1dbc1c4fb67
Sam Beckmann <samvbeckmann@gmail.com>
2016-01-22 17:48:17 -0600

    Fixed bug in overwriting transitions in `SimpleTransitionMap`

--- Project-1/src/com/samvbeckmann/ai/project1/SimpleTransitionMap.java
+++ Project-1/src/com/samvbeckmann/ai/project1/SimpleTransitionMap.java
@@ -4,12 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by sam on 1/22/16.
+ * Transition map with 0.8 chance of going in direction of action,
+ * 0.1 chance of moving in adjacent directions.
+ * Simple implementation of {@link TransitionMap}.
+ *
+ * Note: Assumes 4 actions.
+ *
+ * @author Sam Beckmann
  */
 public class SimpleTransitionMap implements TransitionMap
 {
     private Map<Action, Map<State, Float>> transitions;
 
+    /**
+     * Creates transition map based on bordering states.
+     * If a bordering state does not exist, use current state.
+     *
+     * @param up State bordering above.
+     * @param right State bordering to the right.
+     * @param down State bordering below.
+     * @param left State bordering to the left.
+     */
     public SimpleTransitionMap(State up, State right, State down, State left)
     {
         transitions = new HashMap<Action, Map<State, Float>>();
@@ -19,12 +34,29 @@ public class SimpleTransitionMap implements TransitionMap
         transitions.put(Action.LEFT, makeSimpleTransitionTable(left, down, up));
     }
 
+    /**
+     * Creates transition table from primary and secondary states.
+     * @param main Primary direction, 0.8 change of going towards.
+     * @param firstAlternate Secondary direction, 0.1 chance of going towards.
+     * @param secondAlternate Secondary direction, 0.1 chance of going towards.
+     * @return Transition table, matching subsequent states with probabilities.
+     */
     private Map<State, Float> makeSimpleTransitionTable(State main, State firstAlternate, State secondAlternate)
     {
-        Map<State, Float> transitionTable = new HashMap<State, Float>();
+        Map<State, Float> transitionTable = new HashMap<>();
+
         transitionTable.put(main, 0.8F);
-        transitionTable.put(firstAlternate, 0.1F);
-        transitionTable.put(secondAlternate, 0.1F);
+
+        if (transitionTable.containsKey(firstAlternate))
+            transitionTable.put(firstAlternate, 0.1F + transitionTable.get(firstAlternate));
+        else
+            transitionTable.put(firstAlternate, 0.1F);
+
+        if (transitionTable.containsKey(secondAlternate))
+            transitionTable.put(secondAlternate, 0.1F + transitionTable.get(secondAlternate));
+        else
+            transitionTable.put(secondAlternate, 0.1F);
+
         return transitionTable;
     }
 
