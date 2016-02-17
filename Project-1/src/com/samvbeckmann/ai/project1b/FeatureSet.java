681f1392760b43abe5a31e7fa16839cf633c680c
Sam Beckmann <samvbeckmann@gmail.com>
2016-02-17 11:26:11 -0600

    Added documentation for FeatureSet interface.

--- Project-1/src/com/samvbeckmann/ai/project1b/FeatureSet.java
+++ Project-1/src/com/samvbeckmann/ai/project1b/FeatureSet.java
@@ -3,9 +3,19 @@ package com.samvbeckmann.ai.project1b;
 import java.util.List;
 
 /**
- * Created by sam on 2/15/16.
+ * Defines the features in the world.
+ * Can get the active features at any posiiton in the world.
+ * See {@link DiscreteStates} for an example implementation.
+ *
+ * @author Sam Beckmann
  */
 public interface FeatureSet
 {
+    /**
+     * Gets a list of the features active at a given coordinate.
+     *
+     * @param coordinate Position to get features from
+     * @return List of active features
+     */
     List<Feature> getFeaturesAtCoordinate(Coordinate coordinate);
 }
