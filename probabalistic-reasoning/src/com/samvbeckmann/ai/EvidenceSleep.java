8fae9a46c7c4678ee086e3318f646ec0ea5a8c3f
Sam Beckmann <samvbeckmann@gmail.com>
2016-05-08 19:40:10 -0500

    Added implementation of EvidenceSleep.

--- probabalistic-reasoning/src/com/samvbeckmann/ai/EvidenceSleep.java
+++ probabalistic-reasoning/src/com/samvbeckmann/ai/EvidenceSleep.java
@@ -1,5 +1,6 @@
 package com.samvbeckmann.ai;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -7,26 +8,31 @@ import java.util.Map;
  */
 public class EvidenceSleep implements IEvidence
 {
+    private Map<Integer, Map<String, Boolean>> evidenceHistory;
+
     public EvidenceSleep()
     {
-
+        evidenceHistory = new HashMap<>();
     }
 
     @Override
     public Map<String, Boolean> getEvidenceAtTimestep(int timestep)
     {
-        return null;
+        return evidenceHistory.get(timestep);
     }
 
     @Override
     public void addEvidence(String key, boolean value, int timestep)
     {
-
+        Map<String, Boolean> currentEvidenceAtTimestep = evidenceHistory.get(timestep);
+        currentEvidenceAtTimestep.put(key, value);
+        evidenceHistory.put(timestep, currentEvidenceAtTimestep);
     }
 
     @Override
     public void addEvidence(Map<String, Boolean> pairs, int timestep)
     {
-
+        for (String key : pairs.keySet())
+            addEvidence(key, pairs.get(key), timestep);
     }
 }
